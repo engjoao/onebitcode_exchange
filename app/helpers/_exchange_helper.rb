530faa9d343f1512228a003d34b1b5c9718eb580
João Alberto <aquinosilva@gmail.com>
2017-05-02 23:38:25 -0300

    CoffeScript

--- app/helpers/_exchange_helper.rb
+++ app/helpers/_exchange_helper.rb
@@ -1,6 +1,6 @@
 module ExchangeHelper
   def currency_select
-    retunr[
+    return[
     	'AUD', 'BGN', 'BRL', 'CAD', 'CHF', 'CNY', 'CZK', 'DKK', 'EUR', 'GBP',
       'HKD', 'HRK', 'HUF', 'IDR', 'ILS', 'INR', 'JPY', 'KRW', 'MXN',
       'MYR', 'NOK', 'NZD', 'PHP', 'PLN', 'RON', 'RUB', 'SEK', 'SGD',
